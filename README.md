fcdf58e9751be8f0756927c17db3300b1c80e409
William Lee <william.d.lee90@gmail.com>
2016-05-10 17:25:34 -0700

    [completed] step-2

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-##Solution to step-1
+##Todo
+
+In this exercise, you will create all the necessary RESTful API routes for the ‘lions’ API:
+
+```javascript
+{
+  "GET /lions": {
+    "desc": "returns all lions",
+    "response": "200 application/json",
+    "data": [{}, {}, {}]
+  },
+
+  "GET /lions/:id": {
+    "desc": "returns one lion respresented by its id",
+    "response": "200 application/json",
+    "data": {}
+  },
+
+  "POST /lions": {
+    "desc": "create and returns a new lion uisng the posted object as the lion",
+    "response": "201 application/json",
+    "data": {}
+  },
+
+  "PUT /lions/:id": {
+    "desc": "updates and returns the matching lion with the posted update object",
+    "response": "200 application/json",
+    "data": {}
+  },
+
+  "DELETE /lions/:id": {
+    "desc": "deletes and returns the matching lion",
+    "response": "200 application/json",
+    "data": {}
+  }
+}
+```
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 // TODO: make this work.
-// if yuo go to localhost:3000 the app
+// if you go to localhost:3000 the app
 // there is expected crud to be working here
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -11,6 +11,7 @@ var _ = require('lodash');
 // also, it will server the index.html on the
 // root of that directory on a GET to '/'
 app.use(express.static('client'));
+app.use(bodyParser.json());
 
 // body parser makes it possible to post JSON to the server
 // we can accss data we post on as req.body
@@ -23,5 +24,52 @@ var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
 
-app.listen(3000);
-console.log('on port 3000');
+app.get('/lions', function(req, res) {
+  res.send(lions);
+});
+
+app.get('/lions/:id', function(req, res) {
+  var lion = _.find(lions, {id: req.params.id});
+  res.send(lion || {});
+});
+
+app.post('/lions', function(req, res) {
+  var lion = req.body;
+  id++;
+  lion.id = '' + id;
+
+  lions.push(lion);
+  res.send(lion);
+});
+
+app.put('/lions/:id', function(req, res) {
+  var update = req.body;
+  if(update.id) {
+    delete update.id
+  }
+
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if(!lions[lion]) {
+    res.send('Lion with id of ' + req.params.id + ' does not exist.');
+  } else {
+    var updatedLion = _.assign(lions[lion], update);
+    res.send(updatedLion);
+  }
+});
+
+app.delete('/lions/:id', function(req,res) {
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if(!lions[lion]) {
+    res.send('Lion with id of ' + req.params.id + ' does not exist.');
+  } else {
+    var deletedLion = lions[lion];
+    lions.splice(lion, 1);
+    res.send(deletedLion);
+  }
+});
+
+app.set('port', 3000);
+
+app.listen(app.get('port'), function() {
+  console.log('Listening on port 3000');
+})
